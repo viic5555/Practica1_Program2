5ade34de06f4d8fff0d7aced780917159fc595af
vic <vmartindev9@gmail.com>
2025-02-27 19:21:26 +0100

    Eliminadas las importaciones no usadas de momento.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,5 @@
 package org.example;
 
-import org.example.models.Alumno;
-import org.example.models.Asignatura;
-
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
 }
